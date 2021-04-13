7d6cd684bce2a0796fad9792b4bbeaf953b21de1
shnippi <janjosef.schnyder@bluewin.ch>
2021-04-13 09:03:39 +0200

    changed some plotting

--- helper.py
+++ helper.py
@@ -3,6 +3,7 @@ from IPython import display
 
 plt.ion()
 
+
 def plot(scores, mean_scores):
     display.clear_output(wait=True)
     display.display(plt.gcf())
@@ -13,5 +14,6 @@ def plot(scores, mean_scores):
     plt.plot(scores)
     plt.plot(mean_scores)
     plt.ylim(ymin=0)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show()
