4e665bb5593516bd593241492e74139556607c44
shnippi <jan.schnyder@uzh.ch>
2021-05-15 12:26:45 +0200

    added stuff

---
+++ AC/ac_agent.py
@@ -0,0 +1,7 @@
+
+class Agent():
+    def __init__(self, alpha, beta, input_dims, tau, env,
+                 env_id, gamma=0.99,
+                 n_actions=2, max_size=1000000, layer1_size=256,
+                 layer2_size=256, batch_size=100):
+        pass
